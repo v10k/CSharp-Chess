c607f9f7e40f5dd5532ee9898f57bd3e027db28b
Vinicius Kaiser <vinicius.kaiser43@gmail.com>
2020-01-24 12:39:25 -0300

    Criado a classe Posicao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using tabuleiro;
 
 namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
-           
+            Posicao p = new Posicao(3 , 4);
+
+            Console.WriteLine("Posição: " +p);
+
+            Console.WriteLine();
         }
     }
 }
